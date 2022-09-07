a0025da251d0ff719681128d2be13249b0d7f71f
Bear <danekgdgdg@gmail.com>
2022-09-07 16:26:11 +0300

    [Adaptive] Finished

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,41 @@
 $(document).ready(function () {
-    $(".about__carousel").slick({
+    $(".carousel__inner").slick({
         arrows: false,
         dots: true,
         slidesToShow: 5,
         slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1535,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoint: 1289,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                },
+            },
+        ],
     });
 });
+
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
