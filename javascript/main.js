c675cb79b42a5bb79552f22b213c31ddde5bfde5
Bear <danekgdgdg@gmail.com>
2022-09-11 18:12:29 +0300

    [Revorked] Menu

--- javascript/main.js
+++ javascript/main.js
@@ -33,10 +33,12 @@ $(document).ready(function () {
     });
 }); 
 
+
 function openNav() {
-    document.getElementById("myNav").style.display = "block";
+  document.getElementById("myNav").style.display = "block";
 }
 
+
 function closeNav() {
-    document.getElementById("myNav").style.display = "none";
+  document.getElementById("myNav").style.display = "none";
 }
