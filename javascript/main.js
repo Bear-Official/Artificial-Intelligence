fae82afce9f982517633a3239d7e8c766139b921
Bear <danekgdgdg@gmail.com>
2022-09-14 17:09:24 +0300

    [Added] Owl Carousel

--- javascript/main.js
+++ javascript/main.js
@@ -1,37 +1,25 @@
 $(document).ready(function () {
-    $(".carousel__inner").slick({
-        arrows: false,
-        dots: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        infinite: false,
-        responsive: [
-            {
-                breakpoint: 1535,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                },
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            1600: {
+                items: 5,
             },
-            {
-                breakpoint: 1289,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                },
+            1223: {
+                items: 4,
             },
-
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    adaptiveHeight: true,
-                    variableWidth: true,
-                    centerMode: true,
-                },
+            950: {
+                items: 3,
             },
-        ],
+            528: {
+                items: 2,
+            },
+            0: {
+                items: 1,
+            },
+        },
     });
 });
 
